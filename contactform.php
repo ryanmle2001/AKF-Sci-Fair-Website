f6d0aee9a91b6e56064d186d2499bd93d1479a1e
Patrick Denert <pdenert@gmail.com>
2018-08-02 11:17:05 -0700

    fixes

--- contactform.php
+++ contactform.php
@@ -1,15 +1,15 @@
 <?php
 
-if (isset($_POST['submit'])){
-	$name = $_POST['name'];
-	$mailFrom = $_POST['mail'];
-	$message = $_POST['message'];
+if (isset(_POST['submit'])){
+	name = _POST['name'];
+	mailFrom = _POST['mail'];
+	message = _POST['message'];
 
-	$mailTo = "pdenert@gmail.com";
-	$headers = "From:" .$mailFrom;
-	$txt = "You have received an email from" .$name.  "\n\n" .$message;
+	mailTo = "pdenert@gmail.com";
+	headers = "From:" .mailFrom;
+	txt = "You have received an email from" .name.  "\n\n" .message;
 
-	mail($mailTo, "Science Fair", $txt);
+	mail(mailTo, "Science Fair", txt);
 	header("Location: index.php?mailsend")
 }
 
